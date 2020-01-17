585f52b476c0227baa41b1518e17866b6b18071c
leah1225 <leah871225@gmail.com>
2020-01-17 19:35:56 +0800

    2020/01/17

---
+++ homework/PTTCrawler.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class PttcrawlerSpider(scrapy.Spider):
+    name = 'PTTCrawler'
+    allowed_domains = ['www.ptt.cc']
+    start_urls = ['https://www.ptt.cc/bbs/BeautySalon/M.1579228955.A.D67.html']
+    cookies = {'over18': '1'}
+
+    def start_requests(self):
+    	for url in self.start_urls:
+    		yield scrapy.Request(url=url, callback=self.parse, cookies=self.cookies)
+
+    def parse(self, response):
+        pass
